37e33b50bfaf07e2beb4dbe5b82971d54f8332a3
surajhgh <143310572+surajhgh@users.noreply.github.com>
2025-06-21 22:42:12 +0545

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -26,8 +26,8 @@ export default function Home() {
   <Image
     src="/suraj.webp" 
     alt="Suraj's portfolio picture"
-    width={900}
-    height={1200}
+    width={60}
+    height={120}
     priority={true} // If this is above-the-fold image
   
     quality={85} // Reduces file size while maintaining quality
